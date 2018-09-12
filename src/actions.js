fd36ca5b342b53551ce249153132bd41f16f4fb4
Shant Kevorkian <shant.kevorkian@gmail.com>
2018-09-11 21:46:25 -0700

    initial commit with actions.js

---
+++ src/actions.js
@@ -0,0 +1,26 @@
+export const ADD_TODO                = 'ADD_TODO'
+export const REMOVE_TODO             = 'REMOVE_TODO'
+export const TOGGLE_TODO             = 'TOGGLE_TODO'
+export const SET_VISIBILITY_FILTER   = 'SET_VISIBILITY_FILTER'
+
+export VisibilityFilters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+export function addTodo(text){
+  return {type: ADD_TODO, text}
+}
+
+export function removeTodo(index){
+  return {type: REMOVE_TODO, index}
+}
+
+export function toggleTodo(index){
+  return {type: TOGGLE_TODO, index}
+}
+
+export function setVisibilityFilter(filter) {
+  return {type: SET_VISIBILITY_FILTER, filter}
+}
