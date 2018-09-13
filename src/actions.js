5bd1a2b8415225893b81e583f5ec682ce59546c0
Shant Kevorkian <shant.kevorkian@gmail.com>
2018-09-12 20:25:53 -0700

    added reducers

--- src/actions.js
+++ src/actions.js
@@ -13,9 +13,9 @@ export function addTodo(text){
   return {type: ADD_TODO, text}
 }
 
-export function removeTodo(index){
-  return {type: REMOVE_TODO, index}
-}
+// export function removeTodo(index){
+//   return {type: REMOVE_TODO, index}
+// }
 
 export function toggleTodo(index){
   return {type: TOGGLE_TODO, index}
--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 import {
   ADD_TODO,
-  REMOVE_TODO,
+  // REMOVE_TODO,
   TOGGLE_TODO,
   SET_VISIBILITY_FILTER,
   VisibilityFilters
@@ -12,9 +12,53 @@ const initialState = {
   todos: []
 }
 
-function todoApp(state, action) {
-  if(typeof state === 'undefined'){
-    return initialState
+// state = default
+function todos(state = [], action) {
+  switch(action.type) {
+    case ADD_TODO:
+    return [
+      ...state,
+      {
+        text: action.text,
+        completed: false
+      }
+    ]
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        if(index === action.index) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo
+      })
+    default:
+      return state
   }
-  return state
+}
+
+function todoApp(state = initialState, action) {
+  // state = initialState sets a default value for state
+  switch(action.type) {
+    case SET_VISIBILITY_FILTER:
+      return {
+        // using spread operator to change visibility filter in state
+        ...state,
+        visibilitFilter: action.filter
+      }
+    case ADD_TODO:
+      return {
+        // using spread operator to add new object to state.todos
+        ...state,
+        todos: todos(state.todos, action)
+      }
+    case TOGGLE_TODO:
+      return {
+        ...state,
+        todos: todos(state.todos, action)
+      }
+    default:
+      return state
+  }
 }
